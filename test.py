fa0bfd57cb6f59a1c85e97d5cfe2390ece4d74cd
Bruce7645@gmail.com <Bruce7645@gmail.com>
2020-09-28 20:59:00 +1300

    Display pc specs and time to a txt file

--- test.py
+++ test.py
@@ -2,6 +2,11 @@ from __future__ import print_function
 import sys
 import cv2
 from random import randint
+import platform
+import datetime
+import os
+import psutil #for keeping track of OS
+
 
 kcf_ = cv2.TrackerKCF_create()
 
@@ -17,6 +22,9 @@ success, frame = cap.read()
 if not success:
   print('Failed to read video')
   sys.exit(1)
+
+# Writing a file
+file = open("Test.txt","w")
 
 ## Select boxes
 bboxes = [(500,500,50,50)]
@@ -34,6 +42,7 @@ box = (800, 500, 200, 200)
 box2 = (1449, 35, 92, 163)
 box3 = (879,70,65,164)
 box4 = (1651,123,64,158)
+
 # create a new object tracker for the bounding box and add it
 # to our multi-object tracker
 tracker = cv2.TrackerKCF_create()
@@ -49,8 +58,10 @@ tracker3 = cv2.TrackerKCF_create()
 multiTracker.add(tracker3, frame, box4)
 # trackers.add(tracker, frame, box,trackers)
 
+initalTime = datetime.datetime.now()
 # Process video and track objects
 while cap.isOpened():
+	
 	success, frame = cap.read()
 	if not success:
 		break
@@ -63,13 +74,24 @@ while cap.isOpened():
 		p1 = (int(newbox[0]), int(newbox[1]))
 		p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
 		cv2.rectangle(frame, p1, p2, (0, 255, 0), 2, 1)
-
 	# show frame
 	cv2.imshow('MultiTracker', frame)
 
 	# quit on ESC button
 	if cv2.waitKey(1) & 0xFF == 27:  # Esc pressed
+		
+		FinishTime = datetime.datetime.now()
+		
+		# writing system requirements
+		file.write("Machine (Bit): " + platform.machine() + "\n")
+		file.write("Machine - Version: " + platform.version() +" \n")
+		file.write("Machine - Platform: " + platform.platform() + "\n")
+		file.write("Machine - System: " + platform.system()+ "\n")
+		#overall time
+		FinalTime = str(FinishTime - initalTime)
+		file.write("Time of executation: " + FinalTime +"\n")
 		break
+	
 
 # close all windows
 cv2.destroyAllWindows()
