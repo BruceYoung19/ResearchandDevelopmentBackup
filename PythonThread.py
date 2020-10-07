5e4cb592e82b5646c80b7e9e2be51b339f26ea60
Bruce7645@gmail.com <Bruce7645@gmail.com>
2020-10-07 21:03:16 +1300

    threading has been added

--- PythonThread.py
+++ PythonThread.py
@@ -3,14 +3,6 @@ import threading
 import time
 import random 
 
-#learning how to use a threaded constructor
-def __init__(self,Objid):
-    thread.__init__(self)
-    f = random.random()
-    self.objid = f
-
-def printid(self):
-    print(x.objid)     
 
 def thread_function(name):
     logging.info("Thread %s: starting", name)
@@ -24,7 +16,7 @@ if __name__ == "__main__":
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
     logging.info("Main    : before creating thread")
-    printid(self)
+
     x = threading.Thread(target=thread_function, args=(1,))
     logging.info("Main    : before running thread")
     x.start()
--- muilt-threading.py
+++ muilt-threading.py
@@ -1,14 +1,29 @@
 import random 
+import threading
 
-
-class threads():
+class objs():
+	
 	#constructor
 	def __init__(self):
 		print ("This is the constructor")
-		
+		x = random.randint(0,100)
+		self.object = x
+		print (self.object)
+
+# class for 
+class threads():
+	
+	def chain():
+		k = objs()
+		x = threading.Thread(target=k,args=(1,))
+		#x.start()
+			
+	
+	def run():
 		for i in range(5):
-			x = random.randint(0,100)
-			print (x)
-
-
-newobj = threads()
+			td = threads.chain()
+			print("run has started")
+			
+if __name__ == "__main__":
+	print("main started")
+	threads.run()
